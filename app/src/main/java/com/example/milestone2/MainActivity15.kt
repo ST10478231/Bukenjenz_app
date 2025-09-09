ae17548f6b2a50282d7174efc0d1c864d08a0173
ST10478231 <ST10478231@VCConnect.edu.za>
2025-09-09 13:03:41 +0200

    Changes

--- app/src/main/java/com/example/milestone2/MainActivity15.kt
+++ app/src/main/java/com/example/milestone2/MainActivity15.kt
@@ -11,7 +11,7 @@ class MainActivity15 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main15)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main5)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
